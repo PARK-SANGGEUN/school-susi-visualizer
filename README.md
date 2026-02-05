5dd975793a888d2b3a25dc5ac464c7f46f1277ed
PARK-SANGGEUN <p3412@nate.com>
2026-02-05 16:50:06 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# 수시진학파일 분석기
+
+GitHub Pages용 React 프로젝트
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>수시진학파일 분석기</h1>
+      <p>엑셀 업로드 후 분석 결과를 확인하세요.</p>
+    </div>
+  );
+}
